8adbcc27f954197f2547ab9c35cc188cfbab2dc3
deusyvan <deusyvan@gmail.com>
2018-07-12 00:10:25 -0300

    Variável é usada em qualquer local do sistema fazendo a conexao

--- config.php
+++ config.php
@@ -16,9 +16,10 @@ if(ENVIRONMENT == 'development'){
     $config['dbpass'] = 'admin@12';
 }
 
+global $db;
 try {
     
-    $pdo = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+    $db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
     
 } catch ( PDOException $e) {
     echo "ERRO: ".$e->getMessage();
