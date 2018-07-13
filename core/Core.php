10da9444cb04ddc43fb28bd926b68593159c11c1
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-13 09:49:13 -0300

    Vendo ainda no segundo parametro da url excluindo novamente o primeiro depois de preenchido no array da url

--- core/Core.php
+++ core/Core.php
@@ -14,11 +14,21 @@ class Core {
             array_shift($url);
             
             $currentController = $url[0].'Controller';
+            array_shift($url);
+            
+            if (isset($url[0]) && !empty($url[0])){
+                $currentAction = $url[0];
+            } else {
+                $currentAction = 'index';
+            }
+            print_r($url);
+            
         } else {
             $currentController = 'homeController';
             $currentAction = 'index';
         }
         
+        echo '<hr/>';
         echo 'CONTROLLER: '.$currentController.'<BR/>';
         echo "ACTION: ".$currentAction.'<BR/>';
         
