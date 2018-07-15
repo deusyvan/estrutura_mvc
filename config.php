37530d9ffa4b80600dcebecb47401c07e7a6faed
deusyvan <deusyvan@gmail.com>
2018-07-15 16:48:43 -0300

    Colocando um link como menu entre páginas

--- config.php
+++ config.php
@@ -4,14 +4,14 @@ require 'environment.php';
 $config = array();
 
 if(ENVIRONMENT == 'development'){
-    define("BASE_URL", "http://localhost/estrutura_mvc");
+    define("BASE_URL", "http://localhost/estrutura_mvc/");
     $config['dbname'] = 'estrutura_mvc';
     $config['host'] = 'localhost';
     $config['dbuser'] = 'admin';
     $config['dbpass'] = 'admin@12';
     
 } else {
-    define("BASE_URL", "http://localhost/estrutura_mvc");
+    define("BASE_URL", "http://meusite.com.br/");
     $config['dbname'] = 'estrutura_mvc';
     $config['host'] = 'localhost';
     $config['dbuser'] = 'admin';
--- views/template.php
+++ views/template.php
@@ -5,7 +5,8 @@
 	
 	<body>
 		<h1>Este é o topo</h1>
-		
+		<a href="<?php echo BASE_URL; ?>">Home</a>
+		<a href="<?php echo BASE_URL; ?>galeria">Galeria</a>
 		<hr/>
 		
 		<?php $this->loadViewInTemplate($viewName, $viewData); ?>
