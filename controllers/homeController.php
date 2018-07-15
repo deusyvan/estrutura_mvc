f7a976461a4c7eba32c82bfc0b6249c7401fc2dd
deusyvan <deusyvan@gmail.com>
2018-07-15 16:10:41 -0300

    View recebe os dados e mostra na view

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -4,7 +4,9 @@ class homeController extends controller{
     public function index(){
         
         $dados = array(
-           'quanitdade' => 5 
+           'quantidade' => 5,
+            'nome' => 'Deusyvan',
+            'idade' => '45'
         );
       
         $this->loadView('home', $dados);
--- core/controller.php
+++ core/controller.php
@@ -3,6 +3,7 @@ class controller {
     
     public function loadView($viewName, $viewData = array()){
         
+        extract($viewData);
         require 'views/'.$viewName.'.php';
         
     }
