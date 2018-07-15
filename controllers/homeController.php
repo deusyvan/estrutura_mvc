c68dbdade7fbda1bb0460501f97383192934891c
deusyvan <deusyvan@gmail.com>
2018-07-15 18:10:48 -0300

    Buscando os dados do usuário pelo model

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -3,10 +3,11 @@ class homeController extends controller{
     
     public function index(){
         $anuncios = new Anuncios(); 
+        $usuarios = new Usuarios();
         $dados = array(
            'quantidade' => $anuncios->getQuantidade(),
-            'nome' => 'Deusyvan',
-            'idade' => '45'
+            'nome' => $usuarios->getNome(),
+            'idade' => $usuarios->getIdade()
         );
       
         $this->loadTemplate('home', $dados);
