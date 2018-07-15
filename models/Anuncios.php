15688ab94d3146e43e5dcda0c256bffb9228c693
deusyvan <deusyvan@gmail.com>
2018-07-15 18:25:38 -0300

    Buscando os dados de anuncio pelo banco na criação de um auxiliador o model.php em core

--- models/Anuncios.php
+++ models/Anuncios.php
@@ -1,9 +1,17 @@
 <?php 
-class Anuncios{
+class Anuncios extends model{
     
     public function getQuantidade(){
-        $n = 12;
         
-        return $n;
+        $sql = "SELECT COUNT(*) AS c FROM anuncios";
+        $sql = $this->db->query($sql);
+        
+        if($sql->rowCount() > 0){
+            $sql = $sql->fetch();
+            
+            return $sql['c'];
+        } else {
+            return 0;
+        }
     }
 }
--- script.sql
+++ script.sql
@@ -1,5 +1,16 @@
 CREATE SCHEMA `estrutura_mvc` DEFAULT CHARACTER SET utf8 ;
 
+CREATE TABLE `estrutura_mvc`.`anuncios` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+
+INSERT INTO `estrutura_mvc`.`anuncios` (`titulo`) VALUES ('Teste');
+INSERT INTO `estrutura_mvc`.`anuncios` (`titulo`) VALUES ('Testando 123');
+
+
+
 
 
 
